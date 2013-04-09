45878d0b0bc239e3bfc42c0a4848071d0d9b364a
Brandon Hilkert <brandonhilkert@gmail.com>
2013-04-08 22:00:18 -0400

    Update won deal ticker

--- js/app.js
+++ js/app.js
@@ -12,16 +12,28 @@ App.Deal = DS.Model.extend({
   name: DS.attr('string')
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
+App.Router.map(function() {
+  this.resource('deals', { path: '/deals/:api_key' });
+});
+
+App.DealsRoute = Ember.Route.extend({
+  model: function(params) {
+    // return App.Deal.getWonFor(params.api_key);
     return App.Deal.find();
   }
 });
 
-App.IndexView = Ember.View.extend({
+App.IndexController = Ember.ObjectController.extend({
+  apiKey: "",
+  getDeals: function () {
+    this.transitionToRoute('deals', [this.apiKey]);
+  }
+});
+
+App.DealsView = Ember.View.extend({
   didInsertElement: function() {
     // Add active class to first item
     this.$().find('.item').first().addClass('active');
-    this.$().find('.carousel').carousel({interval: 5000});
+    this.$().find('.carousel').carousel({interval: 1000});
   }
 });
